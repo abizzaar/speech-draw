682418f21822e470fd6a451d3a43c6b7fc1c653f
Abizar Bagasrawala <babyzar97@gmail.com>
2018-11-28 14:41:08 -0600

    second try

--- artyom-abi.js
+++ artyom-abi.js
@@ -1,11 +1,42 @@
 const artyom = new Artyom();
 
-// Add a single command
-var commandHello = {
-  indexes:["hello","good morning","hey"], // These spoken words will trigger the execution of the command
-  action:function(){ // Action to be executed when a index match with spoken word
-      artyom.say("Hey buddy ! How are you today?");
-  }
-};
-
-artyom.addCommands(commandHello);
+(function(window){'use strict';
+    
+    var artyomCommands = [
+        {
+            description:"It is polite to greet people <i class='icon-emoji'></i>",
+            indexes: ['hello how are you','how are you','hello'],
+            action : function(i){
+                var forHowareyou = [
+                    'Thankfully alive and still somewhat young and healthy, in this economy what more can I ask for?',
+                    'Cool as a cucumber',
+                    'I am doing so fabulous today! I can hardly control myself from dancing.',
+                    'From what I hear, I am very good.',
+                    "I can't complain... I've tried, but no one listens.",
+                    "As long as I can keep the kitten I found today, I'll be fine!"
+                ];
+                
+                var forHello = [
+                    "Ring a ding ding, you're talking to the king.",
+                    "Hi, is John there?",
+                    'How goes it?'
+                ];
+                
+                // var i = the index in the array of the given options.
+                switch(i){
+                    case 0:
+                    case 1:
+                        var frase = forHowareyou[Math.floor(Math.random() * forHowareyou.length)];
+                        artyom.say(frase);
+                    break;
+                    case 2:
+                        var frase = forHello[Math.floor(Math.random() * forHello.length)];
+                        artyom.say(frase);
+                    break;
+                }
+            }
+        },
+    ];
+    //Updated to artyom v 0.6
+    artyom.addCommands(artyomCommands);
+})(window);
